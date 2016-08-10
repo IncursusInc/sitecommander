fbeec8088668057fc0d642bd30d9438efecce312
marneman <scott@incursio.com>
2016-08-09 22:37:19 -0400

    Removed local copy of Raphael and now pull it via CDN

--- js/broadcast.js
+++ js/broadcast.js
@@ -1,38 +1,34 @@
+		// Set up broadcasting framework
+		var pusher = new Pusher(drupalSettings.sitecommander.pusherAppKey, { cluster: "ap1", authEndpoint: '/sitecommander/pusherAuth' });
+		var scChannel = pusher.subscribe('site-commander');
+		//var scPresenceChannel = pusher.subscribe('presence-site-commander');
 
-// Set up broadcasting framework
-// TODO - move this to be included in every site page via the module/hook
-var pusher = new Pusher(drupalSettings.sitecommander.pusherAppKey, { cluster: "ap1", authEndpoint: '/sitecommander/pusherAuth' });
-var scChannel = pusher.subscribe('site-commander');
-//var scPresenceChannel = pusher.subscribe('presence-site-commander');
+		pusher.bind('broadcastMessage', function(data) {
 
-pusher.bind('broadcastMessage', function(data) {
-
-	toastr.options = {
-		"closeButton": true,
-		"debug": false,
-		"newestOnTop": false,
-		"progressBar": false,
-		"positionClass": data.messagePosition,
-		"preventDuplicates": false,
-		"onclick": null,
-		"showDuration": "300",
-		"hideDuration": "1000",
-		"timeOut": 0,
-		"extendedTimeOut": 0,
-		"showEasing": "swing",
-		"hideEasing": "linear",
-		"showMethod": "fadeIn",
-		"hideMethod": "fadeOut",
-		"tapToDismiss": false
-	}
-
-	switch(data.messageType) 
-	{
-		case 'info': toastr.info(data.messageBody); break;
-		case 'warning': toastr.warning(data.messageBody); break;
-		case 'error': toastr.error(data.messageBody); break;
-		case 'success': toastr.success(data.messageBody); break;
-	}
-});
-
-
+			toastr.options = {
+				"closeButton": true,
+				"debug": false,
+				"newestOnTop": false,
+				"progressBar": false,
+				"positionClass": data.messagePosition,
+				"preventDuplicates": false,
+				"onclick": null,
+				"showDuration": "300",
+				"hideDuration": "1000",
+				"timeOut": 0,
+				"extendedTimeOut": 0,
+				"showEasing": "swing",
+				"hideEasing": "linear",
+				"showMethod": "fadeIn",
+				"hideMethod": "fadeOut",
+				"tapToDismiss": false
+			}
+		
+			switch(data.messageType) 
+			{
+				case 'info': toastr.info(data.messageBody); break;
+				case 'warning': toastr.warning(data.messageBody); break;
+				case 'error': toastr.error(data.messageBody); break;
+				case 'success': toastr.success(data.messageBody); break;
+			}
+		});
